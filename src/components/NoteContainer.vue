c6b1ef14459a0eca480f26b62fd674e34a297e59
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-22 13:37:37 -0500

    Inital commit with creation of sections in components

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="note-container">
+    <div class="note-selectors">
+      <div class="note-selector active">
+        <p class="note-selector-title">First note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+      <div class="note-selector">
+        <p class="note-selector-title">Second note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+      <div class="note-selector">
+        <p class="note-selector-title">Third note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NoteSelectors from "./NoteSelectors";
+
+export default {
+  components: {
+    NoteSelectors,
+  },
+};
+</script>
